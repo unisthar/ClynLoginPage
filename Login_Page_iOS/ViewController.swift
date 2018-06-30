48470fb2640d3dc9754cc465ecf6fe6f81863314
System Administrator <root@cexs-macbook.local>
2018-06-30 17:01:42 +0100

    Initial Commit

---
+++ Login_Page_iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login_Page_iOS
+//
+//  Created by System Administrator on 30/06/2018.
+//  Copyright © 2018 Usman Nisthar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
